b05a3779e9c5c5697f8a76e6be51d35636aa25a1
kthorn11 <kaitmarie713@gmail.com>
2026-04-01 11:11:21 -0500

    Add files via upload

---
+++ Digital Learning Platform.cpp
@@ -0,0 +1,67 @@
+// Digital Learning Platform.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+
+#include <iostream>
+
+using namespace std;
+
+
+
+class Course
+{
+public:
+    virtual void startClass() = 0;
+};
+
+class ProgrammingCourse : public Course
+{
+
+public:
+    void startClass()
+    {
+        cout << "Programming class has started\n";
+    }
+
+};
+
+
+class MatCourse : public Course
+{
+
+public:void startClass()
+{
+    cout << "Math class has started\n";
+}
+
+};
+
+
+
+int main()
+{
+    Course* course_pointer;
+
+    ProgrammingCourse programming_object;
+    MatCourse math_object;
+
+    course_pointer = &programming_object;
+    course_pointer->startClass();
+
+    course_pointer = &math_object;
+    course_pointer->startClass();
+
+
+    return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
