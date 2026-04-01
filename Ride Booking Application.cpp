3957d75ad231d4b60030efae1cce0f6483024aff
kthorn11 <kaitmarie713@gmail.com>
2026-04-01 11:19:18 -0500

    Add files via upload

---
+++ Ride Booking Application.cpp
@@ -0,0 +1,63 @@
+// Ride Booking Application.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+class Ride
+{
+
+public:
+	virtual void bookRide() = 0;
+	
+};
+
+class BikeRide : public Ride
+{
+public:
+	void bookRide()
+	{
+		cout << "Book ride booked successfully\n";
+	}
+
+
+};
+
+class CarRide : public Ride
+{
+
+public:
+	void bookRide()
+	{
+		cout << "Car ride booked successfully\n";
+	}
+
+
+};
+
+
+int main()
+{
+	Ride *ride_pointer;
+	BikeRide bike_object;
+	CarRide car_object;
+
+	ride_pointer = &bike_object;
+	ride_pointer->bookRide();
+
+	ride_pointer = &car_object;
+	ride_pointer->bookRide();
+
+	return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
